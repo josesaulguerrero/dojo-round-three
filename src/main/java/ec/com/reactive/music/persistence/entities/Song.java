a8597c0db17cbe5b51143e803bb89deb727ca521
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-26 14:42:37 -0500

    Remove useless imports from Song

--- src/main/java/ec/com/reactive/music/persistence/entities/Song.java
+++ src/main/java/ec/com/reactive/music/persistence/entities/Song.java
@@ -1,28 +1,25 @@
 package ec.com.reactive.music.persistence.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalTime;
-import java.util.UUID;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Setter
 @Getter
-@Document(collection = "Song")
-@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="HH:mm:ss")
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@Document(collection = "songs")
 public class Song {
     @Id
     private String idSong;
+    private String albumId;
     private String name;
-    private String idAlbum;
     private String lyricsBy;
     private String producedBy;
     private String arrangedBy;
     private LocalTime duration;
-
 }
