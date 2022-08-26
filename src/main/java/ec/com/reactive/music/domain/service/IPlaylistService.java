743fd0a190fac9230104c8b3a951ef1f4fba8746
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-26 14:53:52 -0500

    Define an interface for Playlist services

---
+++ src/main/java/ec/com/reactive/music/domain/service/IPlaylistService.java
@@ -0,0 +1,23 @@
+package ec.com.reactive.music.domain.service;
+
+import ec.com.reactive.music.persistence.entities.Playlist;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Transactional
+public interface IPlaylistService {
+    Flux<Playlist> findAllPlaylists();
+
+    Mono<Playlist> findPlaylistById(String id);
+
+    Mono<Playlist> savePlaylist(Playlist playlist);
+
+    Mono<Playlist> updatePlaylist(String id, Playlist playlist);
+
+    Mono<Playlist> addSong(String playlistId, String songId);
+
+    Mono<Playlist> remove(String playlistId, String songId);
+
+    Mono<Playlist> deletePlaylist(String id);
+}
