94b861ad7ded6e93f925ba326877942d028bd1c0
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-26 14:43:47 -0500

    Rename target collection in Playlist entity

--- src/main/java/ec/com/reactive/music/persistence/entities/Playlist.java
+++ src/main/java/ec/com/reactive/music/persistence/entities/Playlist.java
@@ -8,16 +8,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Setter
 @Getter
-@Document(collection = "Playlist")
-@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="HH:mm:ss")
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@Document(collection = "playlists")
 public class Playlist {
     @Id
-    private String idPlaylist;
+    private String playlistId;
     private String name;
     private String username;
     private ArrayList<Song> songs;
